79764d531380b674785ce325463ca2396265f81d
Simon <simpa92@icloud.com>
2021-09-11 13:28:54 +0200

    Uppdaterade med funktionalitet för att flytta över HTML-filer till pub-mapp (samt skapa pub-mapp)

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,16 @@
 // Hämta in Gulp och nödvändiga metoder
 const {src, dest} = require('gulp');
+
+// Definiera sökvägar
+const files = {
+    htmlPath: "src/**/*.html"
+}
+
+// Task för att kopiera alla existerande HTML-filer i src-mappen till pub-mappen
+function copyHTML() {
+    return src(files.htmlPath)
+    .pipe(dest('pub'));
+}
+
+// Exportera funktionen för att kopiera HTML-filer till Gulp-default
+exports.default = copyHTML;
