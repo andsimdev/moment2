83a4481a235205f50a9ab7b6f4f07c6eb23c355b
Simon <simpa92@icloud.com>
2021-09-11 14:39:24 +0200

    Lägger till kopiering för alla mappar samt concat för CSS och JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 // Hämta in Gulp och nödvändiga metoder
-const {src, dest} = require('gulp');
+const {src, dest, parallel, series, watch} = require('gulp');
 
-// Definiera sökvägar
+// Hämta in paketet Gulp-concat (för att slå ihop filer)
+const concat = require('gulp-concat');
+
+// Definiera sökvägar för HTML-filer, CSS-filer, JavaScript-filer och bilder
 const files = {
-    htmlPath: "src/**/*.html"
+    htmlPath: "src/**/*.html",
+    cssPath: "src/css/*.css",
+    jsPath: "src/js/*.js",
+    imagePath: "src/images/*"
 }
 
 // Task för att kopiera alla existerande HTML-filer i src-mappen till pub-mappen
@@ -12,5 +18,33 @@ function copyHTML() {
     .pipe(dest('pub'));
 }
 
+// Task för att kopiera alla CSS-filer till pub-mappen, samt slå ihop CSS-filerna
+function copyCSS() {
+    return src(files.cssPath)
+    .pipe(concat('main.css'))
+    .pipe(dest('pub/css'));
+}
+
+// Task för att kopiera alla JavaScript-filer till pub-mappen
+function copyJS() {
+    return src(files.jsPath)
+    .pipe(concat('main.js'))
+    .pipe(dest('pub/js'));
+}
+
+// Task för att kopiera alla bilder till pub-mappen
+function copyImages() {
+    return src(files.imagePath)
+    .pipe(dest('pub/images'));
+}
+
+// Task för att lyssna efter förändringar i källkodsfiler
+function watchTask() {
+    watch([files.htmlPath, files.cssPath, files.jsPath, files.imagePath], parallel(copyHTML, copyCSS, copyJS, copyImages));
+}
+
 // Exportera funktionen för att kopiera HTML-filer till Gulp-default
-exports.default = copyHTML;
+exports.default = series(
+    parallel(copyHTML, copyCSS, copyJS, copyImages),
+    watchTask
+    );
